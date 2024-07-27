f45c1650095936ae0a03da3dffcec8d612b64690
Nicole <nicoliveeira@gmail.com>
2024-07-26 21:55:52 -0300

    Add color radio buttons

--- app/src/main/java/com/example/exercise4/MainActivity.java
+++ app/src/main/java/com/example/exercise4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.exercise4;
 
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
@@ -27,14 +28,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding.imageButton.setOnClickListener(this);
         binding.seekBar.setOnSeekBarChangeListener(this);
         binding.checkNegrito.setOnCheckedChangeListener(this);
-
+        binding.rbAzul.setOnClickListener(this);
+        binding.rbVermelho.setOnClickListener(this);
+        binding.rbVerde.setOnClickListener(this);
         };
+
     @Override
     public void onClick(View view) {
+        if (view.getId() == R.id.imageButton) {
             String texto = String.valueOf(binding.editText.getText());
             binding.textBox.setText(texto);
+        } else if (view.getId() == R.id.rbAzul) {
+            binding.textBox.setTextColor(0xFF0000FF);
+        } else if (view.getId() == R.id.rbVermelho) {
+            binding.textBox.setTextColor(0xFFFF0000);
+        } else if (view.getId() == R.id.rbVerde) {
+            binding.textBox.setTextColor(0xFF00FF00);
+        }
     }
-
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
@@ -64,18 +75,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (binding.checkNegrito.isChecked()) {
             estilo |= Typeface.BOLD;
         }
+
         if (binding.checkItalico.isChecked()) {
             estilo |= Typeface.ITALIC;
         }
 
-        binding.textBox.setTypeface(null, estilo);
-
+        binding.textBox.setTypeface(Typeface.defaultFromStyle(estilo));
         String texto = String.valueOf(binding.editText.getText());
+
         if (binding.checkMaiusculo.isChecked()) {
             texto = texto.toUpperCase();
         }
 
         binding.textBox.setText(texto);
     }
+
 
 }
